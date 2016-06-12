b42029b6be3785f2e0ca2f96f81fe245785a0207
Albert <sero.albert@gmail.com>
2016-06-11 17:58:23 -0700

    Require webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const examples = {
   'oneGif': 'oneGif.cjsx',
